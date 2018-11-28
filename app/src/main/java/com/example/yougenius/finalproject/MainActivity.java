f5b526b8f678f2c83ae11e76ac127ea95f1d4ba9
Youjin Kim <jykm0928@gmail.com>
2018-11-28 15:53:05 +0100

    youjin

--- app/src/main/java/com/example/yougenius/finalproject/MainActivity.java
+++ app/src/main/java/com/example/yougenius/finalproject/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.yougenius.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button)findViewById(R.id.loginButton);
+        register = (Button)findViewById(R.id.registerButton);
+
+        login.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        register.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }

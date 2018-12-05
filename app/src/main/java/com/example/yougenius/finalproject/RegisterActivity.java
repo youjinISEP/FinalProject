f06629e33828f4327a30cb185a8a6f55a4191729
Youjin Kim <jykm0928@gmail.com>
2018-12-05 14:22:30 +0100

    youjin

--- app/src/main/java/com/example/yougenius/finalproject/RegisterActivity.java
+++ app/src/main/java/com/example/yougenius/finalproject/RegisterActivity.java
@@ -1,13 +1,26 @@
 package com.example.yougenius.finalproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends Activity {
 
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        register = (Button)findViewById(R.id.registerButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
